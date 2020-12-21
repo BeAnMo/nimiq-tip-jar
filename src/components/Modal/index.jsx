e59ba5ac8c9d905811b74f75b638ae07556ca433
Ben Morin <bamorin00@gmail.com>
2020-12-21 00:33:00 +0000

    working on modal transitions

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,45 +1,55 @@
+// @ts-check
 import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types'
 
-const Modal = ({ visible, hide }) => {
-    const $self = useRef(null);
-    const display = visible ? '' : 'none';
+import './style.css';
 
-    const handleClose = function handleClose0(e) {
-        if (!$self.current.contains(e.target)) {
-            return hide();
-        }
+const useHideOnOffClickRef = ({ hide, visible }) => {
+  const $self = useRef(null);
+
+  const handleGlobalClose = function handleClose(e) {
+    if (!$self.current.contains(e.target)) {
+      return hide();
     }
+  }
 
-    useEffect(() => {
-        if (visible) {
-            document.body.addEventListener('click', handleClose);
+  useEffect(() => {
+    if (visible) {
 
-            return () => {
-                document.body.removeEventListener('click', handleClose);
-            }
-        }
+      document.body.addEventListener('click', handleGlobalClose);
 
-    }, [visible]);
+      return () => {
+        document.body.removeEventListener('click', handleGlobalClose);
+      }
+    }
+  }, [visible]);
 
-    return (
-        <div
-            ref={$self}
-            className="nq-card"
-            style={{ width: '300px', top: '-50px', left: '-120px', display, position: 'absolute' }}
-        >
-            <div className="nq-card-header">Hello</div>
 
-            <div className="nq-card-body">Body</div>
+  return $self
+}
 
-            <div className="nq-card-footer">Footer</div>
-        </div>
-    );
+const Modal = ({ visible, hide }) => {
+  const clickOffRef = useHideOnOffClickRef({ hide, visible });
+  const display = visible ? '' : 'none';
+
+  return (
+    <div
+      ref={clickOffRef}
+      className="nq-card"
+      style={{ width: '300px', top: '-50px', left: '-120px', display, position: 'absolute' }}
+    >
+      <div className="nq-card-header">Hello <button onClick={hide}>X</button></div>
+
+      <div className="nq-card-body">Body</div>
+
+      <div className="nq-card-footer">Footer</div>
+    </div>
+  );
 }
 
 Modal.propTypes = {
-    visible: PropTypes.bool.isRequired,
-    hide: PropTypes.func.isRequired
+  visible: PropTypes.bool.isRequired,
+  hide: PropTypes.func.isRequired
 }
 
 export default Modal;
